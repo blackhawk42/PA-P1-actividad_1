0ddacde828ce9d29972c8228187869e445e6f4d0
Daniel Chávez <danielsaul96@gmail.com>
2019-02-25 14:08:58 -0600

    Added documentation

--- IntDarray.h
+++ IntDarray.h
@@ -4,11 +4,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * IntDarray is a dynamic array for integers.
+ *
+ * IntDarray_s defines the structure itself, but we'll often work with IntDarray,
+ * which is implemented as a pointer for efficiency's sake.
+ */
 typedef struct {
-	size_t len;
-	int *array;
+	size_t len; /**< Current length of the array. */
+	int *array; /**< Pointer to current physical array in which the data is stored. */
 } IntDarray_s, *IntDarray;
 
+/**
+ * new_darray creates a new IntDarray from an int array with the initial
+ * values.
+ *
+ * @param  init_values Pointer to the array containing the initial values.
+ * @param  init_length Length of the initial array and of the new IntDarray.
+ * @return             The new IntDarray.
+ */
 IntDarray new_darray(int *init_values, size_t init_length) {
 	IntDarray darray = (IntDarray)malloc(sizeof(IntDarray_s));
 
@@ -22,12 +36,20 @@ IntDarray new_darray(int *init_values, size_t init_length) {
 	return darray;
 }
 
+/**
+ * destroy_darray destroys the given IntDarray and frees it's memory.
+ * @param darray The IntDarray to destroy.
+ */
 void destroy_darray(IntDarray darray) {
 	free(darray->array);
 	free(darray);
 }
 
-
+/**
+ * darray_push pushes a new value at the end of a dynamic array.
+ * @param darray The dynamic array to push into.
+ * @param i      The integer to push.
+ */
 void darray_push(IntDarray darray, int i) {
 	darray->array = (int *)realloc(darray->array, sizeof(int)*darray->len+1);
 
@@ -36,7 +58,11 @@ void darray_push(IntDarray darray, int i) {
 	darray->len++;
 }
 
-
+/**
+ * darray_pop pops the last value of the dynamic array.
+ * @param darray The dynamic array to pop from.
+ * @return       The popped integer.
+ */
 int darray_pop(IntDarray darray) {
 	if (darray->len == 0) {
 		return 0;
@@ -51,6 +77,10 @@ int darray_pop(IntDarray darray) {
 }
 
 
+/**
+ * Utility function to print an IntDarray.
+ * @param darray IntArray to print.
+ */
 void print_darray(IntDarray darray) {
 	printf("IntDarray{ ");
 	for(int i = 0; i < darray->len; i++) {
